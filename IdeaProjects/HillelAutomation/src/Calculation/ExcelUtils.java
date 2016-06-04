a365608835b8861fd5f98cc08bd4159393e5b886
AlexGram <agramatik@list.ru>
2016-06-04 23:02:46 +0300

    work with Excel

--- IdeaProjects/HillelAutomation/src/Calculation/ExcelUtils.java
+++ IdeaProjects/HillelAutomation/src/Calculation/ExcelUtils.java
@@ -18,38 +18,47 @@ public class ExcelUtils {
     private static XSSFCell Cell;
 
     private static XSSFRow Row;
-    public static Object [][] getTableArray(String FilePath, String sheetName)throws Exception {
-        Double [][] tabArray = null;
-        try{
+
+    public static Object[][] getTableArray(String FilePath, String sheetName) throws Exception {
+        Double[][] tabArray = null;
+        try {
+
             FileInputStream ExcelFile = new FileInputStream(("C:\\Users\\Грам\\IdeaProjects\\HillelAutomation\\src\\Calculation\\dataTest\\TestData.xlsx"));
             ExcelWBook = new XSSFWorkbook(ExcelFile);
             ExcelWSheet = ExcelWBook.getSheetAt(0);
-            int startRow =1;
-            int startCol =1;
+            int startRow = 1;
+            int startCol = 1;
             int ci, cj;
             int totalRows = ExcelWSheet.getLastRowNum();
-            int totalCols =3;
-            tabArray = new Double [totalRows][totalCols];
-            ci=0;
-            for (int i=startRow;i<=totalRows;i++, ci++){
-                cj=0;
-                for (int j=startCol; j<=totalCols;j++,cj++){
-                    tabArray[ci][cj]=getCellData(i,j);
-                    System.out.println(tabArray[ci][cj]);
+            int totalCols = 3;
+            tabArray = new Double[totalRows][totalCols];
+            for (ci=startRow;ci<totalRows;ci++){
+                for(cj=startCol;cj<totalCols;cj++) {
+                    tabArray[ci][cj] = Cell.getNumericCellValue();
+
                 }
             }
-        }
-        catch (FileNotFoundException e) {
+           /* ci = 0;
+            for (int i = startRow; i <= totalRows; i++, ci++) {
+                cj = 0;
+                for (int j = startCol; j <= totalCols; j++, cj++) {
+                    //tabArray[ci][cj]=getCellData(i,j);
+                    tabArray[ci][cj] = Cell.getNumericCellValue();
+                    System.out.println(tabArray[ci][cj]);
+                }
+            }*/
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not read the Exel sheet");
+            e.printStackTrace();
+        } catch (IOException e) {
             System.out.println("Could not read the Exel sheet");
             e.printStackTrace();
         }
-        catch (IOException e) {
-            System.out.println("Could not read the Exel sheet");
-            e.printStackTrace();
-        }
-        return(tabArray);
+        return (tabArray);
     }
-    public static Double getCellData(int RowNum, int ColNum) throws Exception {
+}
+   /* public static Double getCellData(int RowNum, int ColNum) throws Exception {
         try {
             Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
             int dataType = Cell.getCellType();
@@ -65,4 +74,4 @@ public class ExcelUtils {
 
         }
     }
-}
+}*/
